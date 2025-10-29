4166ddf3788a7b589f58153c777e1c3feb80e3fb
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-10-29 04:01:54 -0700

    project setup

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 3007;
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.post('/submit-order', (req, res) => {
+    res.render('confirmation', { order });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+}); 
